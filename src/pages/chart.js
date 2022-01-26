8a702c3cad2da43b06b55be77ee71c10e1982136
Romaine Allen <romallen1@gmail.com>
2022-01-26 23:09:38 +0900

    Add chart page

---
+++ src/pages/chart.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+import Footer from "../components/Footer"
+
+const ChartPage = () => (
+  <Layout>
+
+    <Footer></Footer>
+  </Layout>
+)
+
+export default ChartPage
