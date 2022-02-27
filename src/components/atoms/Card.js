8ac4a30e143038ca7b385325add431516bfe4002
Romaine Allen <romallen1@gmail.com>
2022-02-27 21:10:17 +0900

    Adjust navbar

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -5,7 +5,7 @@ const Card = ({ heading, paragraph, imgUrl, projectLink, repoLink }) => {
     <div
       className="card"
       style={{
-        backgroundImage:
+        backgroundImage: 
           "linear-gradient(to bottom, rgba(245, 246, 252, 0), rgba(0, 0, 0, 0.2)),url(" +
           imgUrl +
           ")",
