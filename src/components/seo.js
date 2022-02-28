08c13441f8c14b14e77b325e6fb0bfbbaadd698c
Romaine Allen <romallen1@gmail.com>
2022-02-28 20:59:17 +0900

    Add title to webpage

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,6 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
       meta={[
         {
           name: `description`,
@@ -50,23 +49,7 @@ function SEO({ description, lang, meta, title }) {
         {
           property: `og:type`,
           content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.author || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
+        }
       ].concat(meta)}
     />
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from "../components/layout"
-// import SEO from "../components/seo"
+import SEO from "../components/seo"
 // Components
 import Header from "../components/Header"
 import Work from "../components/Work"
@@ -10,7 +10,7 @@ import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
-    {/* <SEO title="Portfolio" /> */}
+    <SEO title="Portfolio" /> 
     <Header></Header>
     <Skills></Skills>
     <Work></Work>
